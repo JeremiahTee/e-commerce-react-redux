1d53bfbca427fab8dada08131cb45458aa9c8d87
Jeremiah <jeremiahteee@gmail.com>
2022-03-01 01:20:43 -0500

    routing + functional catalog page + ProductDetails

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { Container, CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import { AboutPage } from '../../features/about/AboutPage';
 import { Catalog } from '../../features/catalog/Catalog';
+import { HomePage } from '../../features/catalog/home/HomePage';
+import { ProductDetails } from '../../features/catalog/ProductDetails';
+import { ContactPage } from '../../features/contact/ContactPage';
 import { Header } from './Header';
 
 function App() {
@@ -25,7 +30,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/catalog' component={Catalog} />
+        <Route path='/catalog/:id' component={ProductDetails} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/contact' component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,27 +1,103 @@
 import {
   AppBar,
+  Badge,
+  Box,
   FormControlLabel,
+  IconButton,
+  List,
+  ListItem,
   Switch,
   Toolbar,
   Typography,
 } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { NavLink } from 'react-router-dom';
 
 interface Props {
   darkMode: boolean;
   handleThemeChange: () => void;
 }
 
+const midLinks = [
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' },
+];
+
+const rightLinks = [
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' },
+];
+
+const navStyles = {
+  color: 'inherit',
+  typography: 'h6',
+  '&:hover': {
+    color: 'grey.500',
+  },
+  '&.active': {
+    color: 'text.secondary',
+  },
+};
+
 export const Header = ({ darkMode, handleThemeChange }: Props) => {
   return (
     <AppBar position='static' sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography sx={{ mr: 4 }} variant='h6'>
-          RE-STORE
-        </Typography>
-        <FormControlLabel
-          control={<Switch checked={darkMode} onChange={handleThemeChange} />}
-          label='Dark Mode'
-        />
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          <Typography
+            component={NavLink}
+            to='/'
+            exact
+            sx={{
+              mr: 4,
+              color: 'inherit',
+              typography: 'h6',
+              '&:hover': {
+                color: 'grey.500',
+              },
+              '&.active': {
+                color: 'text.secondary',
+              },
+            }}
+            variant='h6'
+          >
+            RE-STORE
+          </Typography>
+          <FormControlLabel
+            control={<Switch checked={darkMode} onChange={handleThemeChange} />}
+            label='Dark Mode'
+          />
+        </Box>
+
+        <List sx={{ display: 'flex' }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+
+        <Box display='flex' alignItems='center'>
+          <List sx={{ display: 'flex' }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+          <IconButton>
+            <Badge badgeContent={4} color='secondary'>
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
