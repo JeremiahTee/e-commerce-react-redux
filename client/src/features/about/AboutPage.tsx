f61968e9c30c092f4aa8dcb547c91f800124d16e
Jeremiah <jeremiahteee@gmail.com>
2022-03-03 10:12:34 -0500

    show validation errors using Alert material-ui component

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup, Container, Typography } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 import agent from '../../app/api/agent';
 
 export const AboutPage = () => {
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+  function getValidationError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log('should not see this')) // the promise is never fulfilled so it will not reach this
+      .catch((error) => setValidationErrors(error));
+  }
+
   return (
     <Container>
       <Typography gutterBottom variant='h2'>
@@ -10,8 +29,11 @@ export const AboutPage = () => {
       <ButtonGroup fullWidth>
         <Button
           variant='contained'
-          onClick={() =>
-            agent.TestErrors.get400Error().catch((error) => console.log(error))
+          onClick={
+            () =>
+              agent.TestErrors.get400Error().catch((error) =>
+                console.log(error)
+              ) // needs arrow function because of catching
           }
         >
           Test 400 Error
@@ -40,17 +62,22 @@ export const AboutPage = () => {
         >
           Test 500 Error
         </Button>
-        <Button
-          variant='contained'
-          onClick={() =>
-            agent.TestErrors.getValidationError().catch((error) =>
-              console.log(error)
-            )
-          }
-        >
+        <Button variant='contained' onClick={getValidationError}>
           Test Validation Error
         </Button>
       </ButtonGroup>
+      {validationErrors.length > 0 && (
+        <Alert severity='error'>
+          <AlertTitle>Validation Errors</AlertTitle>
+          <List>
+            {validationErrors.map((error) => (
+              <ListItem key={error}>
+                <ListItemText>{error}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
     </Container>
   );
 };
