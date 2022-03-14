f63d836b24059a4d431eb3a53aa58e7af32894f0
Jeremiah <jeremiahteee@gmail.com>
2022-03-14 00:14:01 -0400

    fetch basket on app start using context + update basket with item count
    instead of fetching the basket every time the Basket Page is loaded, fetch the basket when loading App ---> also make use of setBasket to update this basket in the context ---> makes use of buyerId from cookie
    
    Basket page will just deconstruct basket from useStoreContext
    
    in the ProductCard, setBasket again to reflect change that item was added ---> this triggers change in BasketPage component --> leading the number of items on icon to be updated

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -12,6 +12,7 @@ import {
 } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link, NavLink } from 'react-router-dom';
+import { useStoreContext } from '../context/StoreContext';
 
 interface Props {
   darkMode: boolean;
@@ -41,6 +42,9 @@ const navStyles = {
 };
 
 export const Header = ({ darkMode, handleThemeChange }: Props) => {
+  const { basket } = useStoreContext();
+  const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <AppBar position='static' sx={{ mb: 4 }}>
       <Toolbar
@@ -91,7 +95,7 @@ export const Header = ({ darkMode, handleThemeChange }: Props) => {
             size='large'
             sx={{ color: 'inherit' }}
           >
-            <Badge badgeContent={4} color='secondary'>
+            <Badge badgeContent={itemCount} color='secondary'>
               <ShoppingCartIcon />
             </Badge>
           </IconButton>
