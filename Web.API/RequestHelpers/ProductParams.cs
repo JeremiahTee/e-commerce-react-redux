51c8fc0452f283e2482a55901d715028288cd93a
Jeremiah <jeremiahteee@gmail.com>
2022-03-31 20:25:35 -0400

    pagination helper classes

---
+++ Web.API/RequestHelpers/ProductParams.cs
@@ -0,0 +1,8 @@
+namespace Web.API.RequestHelpers;
+
+public class ProductParams : PaginationParams
+{
+  public string OrderBy { get; set; }
+  public string SearchTerm { get; set; }
+  public string Brands { get; set; }
+}
